ab56c6f754bed71545ca5fad167a22d728b3ec6a
Vivek Singhal <vivek.singhal@sourcefuse.com>
2023-04-21 18:06:43 +0530

    Second Commit

--- public/index.ts
+++ public/index.ts
@@ -38,7 +38,10 @@ function dateTimeFormatter(target:any,key:string,descriptor:PropertyDescriptor){
 	descriptor.value=function(...args:any[]){
 		const result=originalMethod.apply(this,args);
 		if(result instanceof Date){
-			const formattedDate = result.toISOString().replace(/T/,' ').replace(/\..+/,'');
+			const formattedDate = result.toString()
+			.replace(/T/,' ')
+			.replace(/\..+/,'')
+			;
 			const element=document.getElementById('DateShow');
 			if(element){
 				element.innerHTML=formattedDate;
